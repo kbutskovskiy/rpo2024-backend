f9b5eade1dddfe0f0d28f304a6c61617a4d9129a
kbutskovskiy <kbutskovskiy@mail.ru>
2024-03-29 01:01:44 +0300

    lab4 done in previous commit (init)

--- src/main/java/ru/iu3/backend/controllers/SampleController.java
+++ src/main/java/ru/iu3/backend/controllers/SampleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     @GetMapping("/title")
-    public String getTitle() {
+    public String getTitle(){
         return "<title>Hello from Back-end</title>";
     }
 }
